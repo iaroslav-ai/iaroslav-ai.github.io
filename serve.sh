e03e148d36b6b77a93d7a56c79ab6d704bb90154
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-02-25 13:44:14 +0100

    Add automated installation of dependencies

--- serve.sh
+++ serve.sh
@@ -1,1 +1,5 @@
+sudo apt-get install ruby-dev -y
+sudo gem install jekyll bundler
+printf "source 'https://rubygems.org' \ngem 'github-pages', group: :jekyll_plugins" > Gemfile
+bundle install
 bundle exec jekyll serve
